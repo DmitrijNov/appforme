9a7d11e49fbd21f6b1d58809a70e212f8ccee66f
dnovenkykh@s-pro.io <dnovenkykh@s-pro.io>
2019-11-01 15:19:30 +0200

    fixes with migrations

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class Message(models.Model):
-    sender = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    receiver = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    sender = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='sent_messages'
+    )
+    receiver = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
     text = models.TextField()
 
